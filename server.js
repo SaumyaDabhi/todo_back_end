81b326dba3d7228d978af53849e3de4750fe49ce
SaumyaDabhi <sdabh002@odu.edu>
2023-05-29 19:45:30 -0400

    Changed the origin to the front end URL deployed for CORS Policy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const app =  express();
 
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'https://todo-front-end-rho.vercel.app/', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
