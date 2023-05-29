c5ae15065955c8a4b00e9fd496c7d1562db53a20
SaumyaDabhi <sdabh002@odu.edu>
2023-05-29 18:58:14 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,65 @@
+import express from 'express';
+import cors from "cors";
+
+const app =  express();
+
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions));
+
+let faketodos = [];
+
+app.use(express.json());
+
+app.get('/todos', (req, res) => {
+    res.json(faketodos);
+});
+
+app.post('/todos',(req,res) => {
+    const { text } = req.body;
+    if(text){
+        const newTodo = {
+            id: Date.now(),
+            createdAt: Date.now(),
+            text: text,
+            isCompleted: false
+        }
+        faketodos.push(newTodo);
+        res.status(200).json(newTodo)
+    }
+    else{
+        res.status(400).json({message:'Request body should have a text property'});
+    }
+})
+
+app.delete('/todos/:id', (req,res) => {
+    const removetodo = faketodos.find(todo => todo.id === Number(req.params.id));
+    faketodos =  faketodos.filter(todo => todo.id !== Number(req.params.id));
+    res.status(200).json(removetodo);
+})
+
+app.post('/todos/:id/completed', (req, res) => {
+    const { id } = req.params;
+    const matchingTodo = faketodos.find(todo => todo.id === Number(id));
+
+    const updatedTodo = {
+        ...matchingTodo,
+        isCompleted: true,
+    }
+
+    if (updatedTodo) {
+        faketodos = faketodos.map(todo =>
+            todo.id === Number(id)
+                ? updatedTodo
+                : todo);
+        res.status(200).json(updatedTodo);
+    } else {
+        res.status(400).json({ message: 'There is no todo with that id' });
+    }
+})
+
+app.listen(3001, ()=>{ console.log('Listening on port 3001') });
